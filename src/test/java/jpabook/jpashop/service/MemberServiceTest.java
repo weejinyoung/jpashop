17107345fbb426d1a8c9b9a2dd118d884f668d89
Jn W <a8118199@gmail.com>
2022-05-04 22:10:49 +0900

    회원기능 테스트

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,45 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Rollback(false)
+    public void 회원가입() throws Exception {
+        Member member = new Member();
+        member.setName("wee");
+
+        Long saveId = memberService.join(member);
+
+        assertThat(member).isEqualTo(memberRepository.findOne(saveId));
+    }
+
+    @Test
+    public void 중복회원예외() throws Exception {
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        memberService.join(member1);
+        memberService.join(member2);
+    }
+}

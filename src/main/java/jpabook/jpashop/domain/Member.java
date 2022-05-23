10f45cdc6fdd01709d7899dfca6623a5e4717a4b
Jn W <a8118199@gmail.com>
2022-05-23 18:03:29 +0900

    회원등록 API
    api를 만들때는 엔티티를 파라미터로 받아선 안되고 외부에 노출해선 안된다
    
    외부 api스펙에 맞는 별도의 객체, dto를 만드는게 api정석

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -4,17 +4,20 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter @Setter
+//api를 만들때는 엔티티를 파라미터로 받아선 안되고 외부에 노출해선 안된다
 public class Member {
 
     @Id @GeneratedValue
     @Column(name = "member_id")
     private Long id;
 
+    @NotEmpty
     private String name;
 
     @Embedded

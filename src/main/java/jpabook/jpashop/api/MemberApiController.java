e2244841f47cb073358042667bd404a3d943f97c
Jn W <a8118199@gmail.com>
2022-05-24 15:08:46 +0900

    회원 수정 API
    변경성 메소드인 커맨드와, 쿼리를 철저히 분리하는 방식

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -2,11 +2,10 @@ package jpabook.jpashop.api;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -31,6 +30,17 @@ public class MemberApiController {
         return new CreateMemberResponse(id);
     }
 
+    // 변경성 메소드인 커맨드와, 쿼리를 철저히 분리하는 방식
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(
+            @PathVariable("id") Long id,
+            @RequestBody @Valid UpdateMemberRequest request) {
+
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getName(), findMember.getId());
+    }
+
     @Data
     static class CreateMemberResponse {
         private Long id;
@@ -47,5 +57,17 @@ public class MemberApiController {
         public CreateMemberRequest(String name) {
             this.name = name;
         }
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private String name;
+        private Long id;
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
     }
 }
